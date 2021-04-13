deb1c97bc2d982849d44c5f8035a8e65cbeac201
3trupti <3truptibhuta@gmail.com>
2021-04-13 13:21:57 +0530

    Controller Git_OnlineShoppingAdd .gitignore and .gitattributes.

---
+++ OnlineShoppingVisual/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OnlineShoppingVisual.Models;
+using System.Web.Http.Cors;
+
+namespace OnlineShoppingVisual.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [Route("api/UserApi")]
+    public class UserController : ApiController
+    {
+        Online_ShoppingDBEntities4 db = new Online_ShoppingDBEntities4();
+
+
+        [Route("api/UserApi/InsertCustomer")]
+
+        [HttpPost]
+        public bool Post([FromBody] Customer cus)
+        {
+            Cart car = new Cart();
+            WishList Wish = new WishList();
+            car.Customer_ID = cus.Customer_ID;
+            Wish.Customer_ID=cus.Customer_ID;
+            try
+            {
+
+                db.Customers.Add(cus);
+                db.Carts.Add(car);
+                db.WishLists.Add(Wish);
+                var res = db.SaveChanges();
+                if (res > 0)
+                    return true;
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return false;
+
+        }
+
+        [Route("api/UserApi/InsertCustomer1")]
+
+        [HttpPost]
+        public bool Post([FromBody] Retailer Ret)
+        {
+
+
+            try
+            {
+
+                db.Retailers.Add(Ret);
+                var res = db.SaveChanges();
+                if (res > 0)
+                    return true;
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ OnlineShoppingVisual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineShoppingVisual.WebApiApplication" Language="C#" %>
